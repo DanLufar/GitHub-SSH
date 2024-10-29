e632814035ff745e64d8986ecf18fc2737fac04e
Max <mshevchenko1984@gmail.com>
2024-10-29 13:00:43 +0200

    Add convMiletoKm

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,18 @@
 package app;
 
 public class Main {
-    //private static final double CONVER_km = 1.6;
+    private static final double CONVER_km = 1.6;
 
     public static void main(String[] args) {
-        System.out.println("Distance from Odesa to Kyiv km");
+        System.out.println("Distance from Odesa to Kyiv km.");
+        double ml = 295;
+        double mile = convMileToKm(ml);
+        System.out.println("Distance " + mile + " km.");
 
+    }
+
+    private static double convMileToKm(double ml) {
+        return ml * CONVER_km;
     }
 
 }
