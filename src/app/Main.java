a526b8236a98b9def36b3153a95e378390b8b4d5
Max <mshevchenko1984@gmail.com>
2024-10-29 12:44:56 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    //private static final double CONVER_km = 1.6;
+
+    public static void main(String[] args) {
+        System.out.println("Distance from Odesa to Kyiv km");
+
+    }
+
+}
